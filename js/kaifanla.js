4be5e8f2845fe0f2361675949f2329e7600fce72
ArrayOne <1441576840@qq.com>
2017-01-11 22:21:13 +0800

    进一步提升用户体验（使用ionic的滚动加载ion-infinite-scroll）
    <ion-infinite-scroll
    immediate-check="false"
    <!--阻止加载满屏的默认事件--->
    ng-if="hasMore"
    on-infinite="show()" >
    </ion-infinite-scroll>
    添加ionic的滚动事件，
    几点注意：当滚动刷新后，要代码进行终止无限的刷新
    $scope.$broadcast("scroll.scrollInfiniteComplete");

--- js/kaifanla.js
+++ js/kaifanla.js
@@ -3,21 +3,24 @@
 var app = angular.module('kaifanla', ['ionic']);
 
 
-
 //配置路由模块
-app.config(function($stateProvider,$urlRouterProvider){
+app.config(function ($stateProvider, $urlRouterProvider) {
   $stateProvider
 
-    .state('main',{url:'/main',templateUrl:'tpl/main.html',controller:'mainCtrl'})
+    .state('main', {url: '/main', templateUrl: 'tpl/main.html', controller: 'mainCtrl'})
 
-    .state('myOrder',{url:'/myOrder',templateUrl:'tpl/myOrder.html',controller:'myOrderCtrl'})
-
-    .state('order',{url:'/order/:did',templateUrl:'tpl/order.html',controller:'orderCtrl'})
-
-    .state('detail',{url:'/myDetail/:did',templateUrl:'tpl/detail.html',controller:'detailCtrl'})
+    .state('myOrder', {url: '/myOrder', templateUrl: 'tpl/myOrder.html', controller: 'myOrderCtrl'})
 
 
-    .state('start', {url:'/start',templateUrl:"tpl/start.html", controller:"startCtrl"})
+    /*不能为同一状态配置两个模块，状态名重复了*/
+    //.state('order',{url:'/order',templateUrl:'tpl/order.html',controller:'orderCtrl'})
+
+    .state('order', {url: '/order/:did', templateUrl: 'tpl/order.html', controller: 'orderCtrl'})
+
+    .state('detail', {url: '/myDetail/:did', templateUrl: 'tpl/detail.html', controller: 'detailCtrl'})
+
+
+    .state('start', {url: '/start', templateUrl: "tpl/start.html", controller: "startCtrl"})
 
   $urlRouterProvider.otherwise('start');//异常情况的处理
 
@@ -25,108 +28,118 @@ app.config(function($stateProvider,$urlRouterProvider){
 
 
 //父级控制器
-app.controller('parentCtrl',['$scope','$state',function($scope,$state){
-  $scope.jump=function(arg,data){
-    $state.go(arg,data);
+app.controller('parentCtrl', ['$scope', '$state', function ($scope, $state) {
+  $scope.jump = function (arg, data) {
+    $state.go(arg, data);
   }
 }]);
 
-app.controller('detailCtrl',['$scope','$http','$stateParams',function($scope,$http,$stateParams){
-debugger;
+app.controller('detailCtrl', ['$scope', '$http', '$stateParams', function ($scope, $http, $stateParams) {
+  debugger;
   console.log($stateParams.did);
   $http.
     get('data/dish_getbyid.php?id=' + $stateParams.did)
-    .success(function(data){$scope.dish=data[0];
-  //  console.log($scope.dish);
-  });
+    .success(function (data) {
+      $scope.dish = data[0];
+      //  console.log($scope.dish);
+    });
 }]);
 
 //main 的控制器
-app.controller('mainCtrl',['$scope','$http',function($scope,$http){
+app.controller('mainCtrl', ['$scope', '$http', '$ionicLoading', function ($scope, $http, $ionicLoading) {
   //$scope.$watch('kw',function(){
   //  console.log($scope.kw);
   //});
 
 
   //在用户打开界面的时候要显示数据
-  $http.get("data/dish_getbypage.php?start=0").success(function(data){
+  $http.get("data/dish_getbypage.php?start=0").success(function (data) {
     //console.log(data);
-    $scope.dishList=data;
+    $scope.dishList = data;
   });
 
-   //监听用户的输入，经行操作。
-  $scope.inputTxt={kw:''};
-  $scope.$watch('inputTxt.kw',function(){
-    if($scope.inputTxt.kw){//只有有意义的关键字才经行操作
-      $http.get('data/dish_getbykw.php?kw='+$scope.inputTxt.kw).success(function(data){
+  //监听用户的输入，经行操作。
+  $scope.inputTxt = {kw: ''};
+  $scope.$watch('inputTxt.kw', function () {
+    if ($scope.inputTxt.kw) {//只有有意义的关键字才经行操作
+      $http.get('data/dish_getbykw.php?kw=' + $scope.inputTxt.kw).success(function (data) {
 
-        $scope.dishList=data;
+        $scope.dishList = data;
       });
     }
 
- });
-  $scope.hasMore=true;
+  });
+  $scope.hasMore = true;
   //当用户点击，加载更多的按钮，就查找
- $scope.loadMore=function(){
 
-   $http
-     .get('data/dish_getbypage.php?start='+ $scope.dishList.length)
-     .success(function(data){
+  $scope.loadMore = function () {
+    $http
+      .get('data/dish_getbypage.php?start=' + $scope.dishList.length)
+      .success(function (data) {
 
-       if(data.length<5){//如果返回的数据小于5条就进行判断
-         $scope.hasMore=false;
+        if (data.length < 5) {//如果返回的数据小于5条就进行判断
+          $scope.hasMore = false;
 
-       }
-       $scope.dishList=  $scope.dishList.concat(data);
-       console.log(data);
-
-    //   $scope.$broadcast("scroll.scrollInfiniteComplete");
-     });
-
- }
-
-
-}]);
-
-
-
-app.controller('myOrderCtrl',['$scope','$http',function($scope,$http){
-   $http
-     .get('data/order_getbyphone.php?phone='+sessionStorage.getItem('phone')).success(function(data){
-     console.log(data);
-     $scope.orderList=data;
-   });
-
-}]);
-
-app.controller('orderCtrl',['$scope','$http','$stateParams',
-  '$httpParamSerializerJQLike',function($scope,$http,$stateParams,$httpParamSerializerJQLike){
-
-    $scope.order={did : $stateParams.did};
-
-  $scope.submitOrder=function(){
-
-    var  orderData=$httpParamSerializerJQLike($scope.order);
-  console.log($stateParams.id);
-  console.log(orderData);
-
-    $http.get("data/order_add.php?"+orderData).success(function(data){
-
-      console.log(data);
-
-      if( data[0].msg=='succ'){
-
-        $scope.succMsg="订单已经下达";
-        sessionStorage['phone']=$scope.order.phone;
-      }
-
-      else{
-        $scope.errMsg="请完善信息....";
-      }
-    });
+        }
+        /*利用concat经行数组的拼接（因为返回的是对象数组）*/
+        $scope.dishList = $scope.dishList.concat(data);
+        /*阻止无限的刷新，刷新之后就终止刷新*/
+        $scope.$broadcast("scroll.scrollInfiniteComplete");
+      });
   }
+
+  $scope.show = function () {/*/显示*/
+    $ionicLoading.show({
+      template: '正在加载.....'
+    });
+    setTimeout($scope.hide,2000);
+  };
+
+  $scope.hide=function(){/*隐藏*/
+    $ionicLoading.hide();
+    $scope.loadMore();
+  }
+
 }]);
 
-app.controller('startCtrl',['$scope','$http',function($scope,$http){
+
+app.controller('myOrderCtrl', ['$scope', '$http', function ($scope, $http) {
+  $http
+    .get('data/order_getbyphone.php?phone=' + sessionStorage.getItem('phone')).success(function (data) {
+      console.log(data);
+      $scope.orderList = data;
+    });
+
+}]);
+
+app.controller('orderCtrl', ['$scope', '$http', '$stateParams',
+  '$httpParamSerializerJQLike', function ($scope, $http, $stateParams, $httpParamSerializerJQLike) {
+
+    $scope.order = {did: $stateParams.did};
+
+    $scope.submitOrder = function () {
+
+      var orderData = $httpParamSerializerJQLike($scope.order);
+      console.log($stateParams.id);
+      console.log(orderData);
+
+      $http.get("data/order_add.php?" + orderData).success(function (data) {
+
+        console.log(data);
+
+        if (data[0].msg == 'succ') {
+
+          $scope.succMsg = "订单已经下达";
+          sessionStorage['phone'] = $scope.order.phone;
+        }
+
+        else {
+          $scope.errMsg = "请完善信息....";
+        }
+      });
+    }
+  }]);
+
+app.controller('startCtrl', ['$scope', '$http', function ($scope, $http) {
 
 }]);
